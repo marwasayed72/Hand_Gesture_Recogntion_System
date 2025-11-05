a31d2e58af422f50a0899360468770c2407fb7ed
marwasayed72 <marwasaysal@gmail.com>
2025-11-06 00:08:12 +0200

    Refactor model loading to use dynamic path

--- streamlit_app/model_utils.py
+++ streamlit_app/model_utils.py
@@ -2,8 +2,9 @@ import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
 
-
-model = load_model("best.h5")
+model_path = os.path.join(os.path.dirname(__file__), "best.h5")
+model = load_model(model_path)
+#model = load_model("best.h5")
 
 
 labels = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
@@ -24,5 +25,6 @@ def predict_gesture(frame):
 
     class_index = np.argmax(prediction)
     predicted_label = labels[class_index]
+
 
     return predicted_label
