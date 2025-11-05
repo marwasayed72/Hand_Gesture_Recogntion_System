5ff1a6ea6dd3c2b4258b3e6485dc67a44154a394
marwasayed72 <marwasaysal@gmail.com>
2025-11-06 00:43:24 +0200

    Update capture_frame to handle capture failure
    Modified capture_frame function to return None for both values when frame capture fails.

--- streamlit_app/camera_utils.py
+++ streamlit_app/camera_utils.py
@@ -19,14 +19,14 @@ def capture_frame():
 
     if not ret:
         print("❌ Failed to capture frame.")
-        return None
+        return None, None  # بدل return None بس
 
-    # Define ROI (Region of Interest)
+    # ROI
     x1, y1, x2, y2 = 100, 100, 300, 300
     roi = frame[y1:y2, x1:x2]
 
-    # Preprocess for model
+    # Preprocess
     processed = preprocess_frame(roi, img_size=64, grayscale=True)
     processed = np.expand_dims(processed, axis=0)
 
-    return roi, processed  # Return both: raw image (for display) + processed image (for model)
+    return roi, processed
